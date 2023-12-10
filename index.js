0e6248dac4e301c162d62b30d821634743745b81
Maxim Svishech <Maxim.Svishech@cellebrite.com>
2023-12-10 15:50:01 +0200

    remove current location

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import axios from "axios";
-import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,6 +11,7 @@ const apiKey = process.env.API_KEY;
 const app = express();
 const port = 3000;
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -63,47 +63,6 @@ app.post("/city", async (req, res) => {
   }
 });
 
-app.post("/current", async (req, res) => {
-  const locationData = await getUserLocation();
-  const city = locationData.city;
-  const country = locationData.country;
-  const ipAddress = req.ip;
-
-  console.log('User IP:', ipAddress);
-  const options = {
-    method: "GET",
-    url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
-    params: {
-      q: `${city},${country}`,
-      days: "3",
-    },
-    headers: {
-      "X-RapidAPI-Key": apiKey,
-      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-    },
-  };
-  try {
-    const response = await axios.request(options);
-    const jsonData = response.data;
-    res.render("index.ejs", { content: jsonData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch weather data" });
-  }
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-async function getUserLocation() {
-  try {
-    const response = await axios.get("https://ipinfo.io/json");
-    const data = response.data;
-
-    const { city, country } = data;
-    return { city, country };
-  } catch (error) {
-    console.error("Error fetching location:", error.message);
-  }
-}
